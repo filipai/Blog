fa682e5b469c2afef1e1a2a25cf3bae6471146ed
filipai <filipailiev@gmail.com>
2016-08-31 22:31:48 +0300

    Added filtering and paging to the homepage

--- MVC-Blog/MVC-Blog/Controllers/HomeController.cs
+++ MVC-Blog/MVC-Blog/Controllers/HomeController.cs
@@ -4,16 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace MVC_Blog.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string currentFilter, string searchString, int? page)
         {
             var db = new ApplicationDbContext();
-            var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
-            return View(post.ToList());
+            var post = db.Posts.OrderByDescending(p => p.Date).Take(db.Posts.Count());
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                post = post.Where(s => s.Title.Contains(searchString)
+                                       || s.Body.Contains(searchString));
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(post.ToPagedList(pageNumber, pageSize));
+
+            //return View(post.ToList());
         }
     }
 }
